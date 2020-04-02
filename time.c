f834c96db9850731275628cc043cd15ea96024cd
Francesco <zucchi@LAPTOP-D3JH8BM2.localdomain>
2020-04-02 12:47:40 +0200

    nothing

--- time.c
+++ time.c
@@ -12,21 +12,22 @@ int main() {
         end = clock;
     } while (end == start);
     clock_t resolution = end - start;
+    int e = 0;
 
-    //2) ε = 1%
+    //2) e = 1%
     //prima devo prendere ti = tempo inizializzaione vettore
-    //==> tempo totale >= resolution * (1/ε + 1)
+    //==> tempo totale >= resolution * (1/e + 1)
     int count = 0;
     clock_t start = clock;
-    /*do {
+    do {
         inizializza(); //meglio cambiare array disordinato ogni volta
         esegui();
         end = clock();
         count++;
-    } while (end - start <= resolution * (1/e + 1));*/
-    int tinit = 0;
-    int t = (end - start) / count;
-    int t2 = t - tinit;
+    } while (end - start <= resolution * (1/e + 1));
+    int tInizializzazione = 0;
+    int tTotale = (end - start) / count;
+    int tEsecuzione = tTotale - tInizializzazione;
 
     //3) scomputo inizializzazione
 
