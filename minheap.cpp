7c0bfd94d2f3bcfb2f357236b52afe6747b031f5
Francesco <zucchi@LAPTOP-D3JH8BM2.localdomain>
2020-04-25 17:39:12 +0200

    fixed heap select bug bro

--- minheap.cpp
+++ minheap.cpp
@@ -2,23 +2,9 @@
 #include "minheap.h"
 //#include "support_minheap.h"
 
-int heapSelect(MinHeap* h1, supportMeanHeap* h2, int k) {
-    int root = h1->getRoot();
-    h2->insert(root,0);       //inserisco la radice della prima MinHeap nella seconda MinHeap
-    for (int i = 0; i < k-1; ++i) {
-        int newKeyPosition = h2->extractPos();
-        if (newKeyPosition <= (h1->heapsize/2)-1) {
-            h2->insert(h1->vec[h1->leftSon(newKeyPosition)], h1->leftSon(newKeyPosition));
-            h2->insert(h1->vec[h1->rightSon(newKeyPosition)], h1->rightSon(newKeyPosition));
-        }
-        assert(isMinHeap(h1));
-        assert(isMinHeapSupport(h2));
-    }
-    int last = h2->nodePos[0].first;
-    return last;
-}
-
 //9 5 2 6 3
+
+int heapSelect(MinHeap* h1, supportMeanHeap* h2, int k);
 
 int main() {
     vector<int> keys;
@@ -30,4 +16,23 @@ int main() {
     h1.buildMinHeap();
     cout << heapSelect(&h1, &h2, key);
     return 0;
+}
+
+int heapSelect(MinHeap* h1, supportMeanHeap* h2, int k) {
+    int root = h1->getRoot();
+    h2->insert(root,0);       //inserisco la radice della prima MinHeap nella seconda MinHeap
+    for (int i = 0; i < k-1; ++i) {
+        int newKeyPosition = h2->extractPos();
+        //if (newKeyPosition <= (h1->heapsize/2)-1) {
+        if(h1->leftSon(newKeyPosition) <= h1->heapsize) {
+            h2->insert(h1->vec[h1->leftSon(newKeyPosition)], h1->leftSon(newKeyPosition));
+            if(h1->rightSon(newKeyPosition) <= h1->heapsize) {
+                h2->insert(h1->vec[h1->rightSon(newKeyPosition)], h1->rightSon(newKeyPosition));
+            }
+        }
+        assert(isMinHeap(h1));
+        assert(isMinHeapSupport(h2));
+    }
+    int last = h2->nodePos[0].first;
+    return last;
 }
